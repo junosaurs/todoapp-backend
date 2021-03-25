32effcff751013a58e923fb330cf7ed51e1cade1
cunning-seal <a.titov9996@gmail.com>
2021-03-25 18:01:57 +0300

    необычный db url

--- database/session.py
+++ database/session.py
@@ -9,7 +9,8 @@ from sqlalchemy import create_engine
 configuration = os.environ.get("CONFIG")
 if configuration == "HEROKU":
     # подключение к Heroku Postgres
-    url = os.environ.get("DATABASE_URL")
+    
+    url = "postgresql" + os.environ.get("DATABASE_URL")[8:]
 else:
     # подключение на тестовом сервере
     from config import settings
