8fc25766fc8296b8bd0c21d4d4e83477d1a0f985
cunning-seal <a.titov9996@gmail.com>
2021-04-01 18:20:51 +0300

    Revert "внес изменения в CORS-настройки для доступа из SPA"
    This reverts commit 5f81e0ee54186ad1fb2e53758ba72ace46994968.

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from typing import List
 from fastapi import FastAPI, Depends, HTTPException, Response, status
-from fastapi.middleware.cors import CORSMiddleware
 import crud
 import schemas
 from database.session import SessionLocal, engine
@@ -15,15 +14,6 @@ def get_session():
         session.close()
 
 app = FastAPI()
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"]
-    allow_headers=["*"]
-)
-
-
 Base.metadata.create_all(engine)
 
 # базовая точка входа
